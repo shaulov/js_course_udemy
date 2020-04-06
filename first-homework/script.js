885b42a703f1014e333f944427f66a114e01f466
Skinny <ivan.shaulov@mail.ru>
2020-04-06 20:40:46 +0300

    modified js file

--- first-homework/script.js
+++ first-homework/script.js
@@ -1,23 +1,25 @@
 "use strict";
 
 let money = prompt("Ваш бюджет на месяц?", ""),
-  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
-  budget: money,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  incomes: [],
-  saving: false,
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    incomes: [],
+    saving: false,
 };
 
 let expenditures = prompt(
-    "Введите обязательную статью расходов в этом месяце",
-    ""
-  ),
-  howItCost = prompt("Во сколько обойдётся?", "");
+        "Введите обязательную статью расходов в этом месяце",
+        ""
+    ),
+    howItCost = prompt("Во сколько обойдётся?", "");
 
-console.log(appData.expenses[expenditures]);
+appData.expenses[expenditures] = howItCost;
+
+console.log(appData.expenses);
 
 alert(appData.budget / 30);
